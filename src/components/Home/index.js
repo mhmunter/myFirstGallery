88d1abeac8c1bca1693d06e066151b4ac124cc56
mhmunter <mark.munter1@gmail.com>
2022-09-15 10:12:57 -0500

    changed background

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -20,14 +20,15 @@ return (
       ></img>
       <p>
         {" "}
-        I was born and raised in San Antonio Texas. My famliy is well known in
-        the local music and art communties. I earned a B.A. in History with a
-        minor in Geography.My wife is also a software engineer, and my son is
-        currently in Kindergarten. I've spent the majority of my professional
-        life in the banking indusrty, with an emphasis on customer service. I'm
-        a lover of puzzles detactive novels, and history. In genral I have a
+        I was born and raised in San Antonio Texas. My family is well known in
+        the local music and art communities. I earned a B.A. in History with a
+        minor in Geography. My wife is also a software engineer, and my son is
+        currently in kindergarten. I've spent the majority of my professional
+        life in the banking industry, with an emphasis on customer service. I'm
+        a lover of puzzles detective novels, and history. In general I have a
         passion for learning, searching for answers, and finding the truth in
         all things.
+
       </p>
       <div className="pRow">
         <img
